49b1c6a7f679369e52f4fc06ef2e9ccf677da8c7
ngdnghia28 <ngd.nghia28@gmail.com>
2021-09-17 20:31:30 +0700

    refactor company profile

--- src/company-profiles/dto/create-company-profile.dto.ts
+++ src/company-profiles/dto/create-company-profile.dto.ts
@@ -1,30 +1,80 @@
 import { OmitType } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString, IsUrl, IsUUID } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsUUID,
+} from 'class-validator';
+import { Time } from '../entities/company-profile.entity';
 
 export class CreateCompanyProfileDto {
   @IsUUID()
   userId: string;
 
+  @IsString()
+  company: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
   @IsEmail()
   email: string;
 
+  @IsString()
+  title: string;
+
   @IsUrl()
   @IsOptional()
-  linked_in?: string;
+  linkedIn?: string;
+
+  @IsUrl()
+  @IsOptional()
+  website?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  position?: string;
+
+  @IsEnum(Time)
+  @IsOptional()
+  time?: Time;
+
+  @IsString()
+  @IsOptional()
+  headCount?: string;
+
+  @IsString()
+  @IsOptional()
+  saleTools?: string;
+
+  @IsString()
+  @IsOptional()
+  targetCustomer?: string;
+
+  @IsString()
+  @IsOptional()
+  teamSize?: string;
 
   @IsUrl()
   @IsOptional()
   avatar?: string;
 
   @IsString()
-  headline: string;
+  @IsOptional()
+  headline?: string;
 
   @IsString()
-  description: string;
-
-  @IsUrl()
   @IsOptional()
-  website?: string;
+  description?: string;
 }
 
 export class CreateMyCompanyProfileDto extends OmitType(
--- src/company-profiles/entities/company-profile.entity.ts
+++ src/company-profiles/entities/company-profile.entity.ts
@@ -2,6 +2,11 @@ import { Base } from 'src/core/base.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
+export enum Time {
+  FULLTIME = 'FULLTIME',
+  PARTTIME = 'PARTTIME',
+  BOTH = 'BOTH',
+}
 @Entity('company_profiles')
 export class CompanyProfile extends Base {
   @Column({
@@ -14,7 +19,19 @@ export class CompanyProfile extends Base {
   user: User;
 
   @Column()
+  company: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
   email: string;
+
+  @Column()
+  title: string;
 
   @Column({
     nullable: true,
@@ -24,18 +41,58 @@ export class CompanyProfile extends Base {
   @Column({
     nullable: true,
   })
-  avatar?: string;
-
-  @Column()
-  headline: string;
-
-  @Column({
-    length: 1023,
-  })
-  description: string;
+  website?: string;
 
   @Column({
     nullable: true,
   })
-  website?: string;
+  phone?: string;
+
+  @Column({
+    nullable: true,
+  })
+  position?: string;
+
+  @Column({
+    type: 'enum',
+    enum: Time,
+    nullable: true,
+  })
+  time?: Time;
+
+  @Column({
+    nullable: true,
+  })
+  headCount?: string;
+
+  @Column({
+    nullable: true,
+  })
+  saleTools?: string;
+
+  @Column({
+    nullable: true,
+  })
+  targetCustomer?: string;
+
+  @Column({
+    nullable: true,
+  })
+  teamSize?: string;
+
+  @Column({
+    nullable: true,
+  })
+  avatar?: string;
+
+  @Column({
+    nullable: true,
+  })
+  headline?: string;
+
+  @Column({
+    length: 1023,
+    nullable: true,
+  })
+  description?: string;
 }
