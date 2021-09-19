c91958444460a329896d8b1e206d6f5b14c7ca74
ngdnghia28 <ngd.nghia28@gmail.com>
2021-09-19 21:36:17 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Sale pipe API document')
     .setDescription('Sale pipe API document')
